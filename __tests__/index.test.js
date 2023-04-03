9340f18a6688603949b13cb661a34753eab2ac93
Dom Harrington <domharrington@protonmail.com>
2023-04-03 14:22:31 +0100

    fix: try to fix tests on github actions

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,13 +7,20 @@ const action = require('../');
 
 it('should upload specs to micro', async () => {
   const mock = nock('https://micro.readme.build')
+    // Removing some properties on the body because
+    // these are undefined on localhost, but populated
+    // when running these tests in GitHub CI
+    .filteringRequestBody(body => {
+      const _body = JSON.parse(body);
+      delete _body.payload;
+      delete _body.runId;
+      return JSON.stringify(_body);
+    })
     .post('/api/uploadSpec', JSON.stringify({
       oas: {
         fileName: "__tests__/__fixtures__/petstore.json",
         oas: petstore,
       },
-      runId: null,
-      payload: {},
     }))
     .reply(200, JSON.stringify({ url: 'https://example.com', explanation: 'Lorem ipsum' }));
 
