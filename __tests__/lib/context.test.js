e8a069d2834887f8d7f9f23bdc30c709a81e0b27
Dom Harrington <domharrington@protonmail.com>
2023-05-04 13:44:11 +0100

    feat: add support for fetching context from bitbucket pipelines

--- __tests__/lib/context.test.js
+++ __tests__/lib/context.test.js
@@ -1,7 +1,20 @@
-const getContext = require('../../lib/context');
+const { execSync } = require('child_process');
+
 const githubPayload = require('../__fixtures__/github-payload.json');
 
 describe('getContext()', () => {
+  // https://stackoverflow.com/a/48042799
+  const OLD_ENV = process.env;
+
+  beforeEach(() => {
+    jest.resetModules();
+    process.env = { ...OLD_ENV };
+  });
+
+  afterAll(() => {
+    process.env = OLD_ENV;
+  });
+
   describe('github', () => {
     it('should pull `context` from environment variables', () => {
       // https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
@@ -13,6 +26,9 @@ describe('getContext()', () => {
       process.env.GITHUB_RUN_ID = '1234';
       process.env.GITHUB_EVENT_PATH = `${__dirname}/../__fixtures__/github-payload.json`;
 
+      // eslint-disable-next-line global-require
+      const getContext = require('../../lib/context');
+
       expect(getContext()).toMatchObject({
         ref: process.env.GITHUB_REF,
         sha: process.env.GITHUB_SHA,
@@ -20,14 +36,39 @@ describe('getContext()', () => {
         runId: parseInt(process.env.GITHUB_RUN_ID, 10),
         payload: githubPayload,
       });
+    });
+  });
 
-      // Clean up the env after we've modified it
-      delete process.env.GITHUB_ACTIONS;
-      delete process.env.GITHUB_REF;
-      delete process.env.GITHUB_SHA;
-      delete process.env.GITHUB_ACTOR;
-      delete process.env.GITHUB_RUN_ID;
-      delete process.env.GITHUB_EVENT_PATH;
+  describe('bitbucket', () => {
+    it('should pull `context` from environment variables', () => {
+      // https://support.atlassian.com/bitbucket-cloud/docs/variables-and-secrets/
+      process.env.BITBUCKET_COMMIT = 'e32041305b8573674b6f85068ee95591029f58a0';
+      process.env.BITBUCKET_STEP_TRIGGERER_UUID = 'domharrington';
+      process.env.BITBUCKET_BUILD_NUMBER = '1234';
+      process.env.BITBUCKET_REPO_SLUG = 'repo-name';
+
+      // eslint-disable-next-line global-require
+      const getContext = require('../../lib/context');
+
+      expect(getContext()).toMatchObject({
+        ref: execSync('git rev-parse --abbrev-ref HEAD', { encoding: 'utf-8' }).trim(),
+        sha: process.env.BITBUCKET_COMMIT,
+        actor: process.env.BITBUCKET_STEP_TRIGGERER_UUID,
+        runId: parseInt(process.env.BITBUCKET_BUILD_NUMBER, 10),
+        payload: {
+          repository: {
+            name: process.env.BITBUCKET_REPO_SLUG,
+          },
+          commits: [
+            {
+              message: 'test: attempt to fix tests in CI env',
+              author: {
+                username: process.env.BITBUCKET_STEP_TRIGGERER_UUID,
+              },
+            },
+          ],
+        },
+      });
     });
   });
 });
